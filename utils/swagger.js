1d737cd56dbe80f817e8cd6dd15856b090ac2ed3
totrieuvy <vytotrieu12a121@gmail.com>
2025-02-16 13:47:43 +0700

    sua file swagger

--- utils/swagger.js
+++ utils/swagger.js
@@ -12,7 +12,7 @@ const options = {
     },
     servers: [
       {
-        url: "https://mma301-0au5.onrender.com/", // Base URL for your API
+        url: "https://mma-skincare.onrender.com", // Base URL for your API
       },
     ],
     components: {
